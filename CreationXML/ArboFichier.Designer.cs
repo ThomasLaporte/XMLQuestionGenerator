3874e19c75f1e20264a7c9e83986c02d998190ab
unknown <t.laporte@INFO-LAPORTE.DomCERAL.local>
2016-03-30 17:34:09 +0200

    Ajout du TreeView Update d'un noeud

---
+++ CreationXML/ArboFichier.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CreationXML
+{
+    partial class ArboFichier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(632, 343);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(12, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Double cliquez sur la question que vous souhaitez modifier.";
+            // 
+            // ArboFichier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(656, 394);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "ArboFichier";
+            this.Text = "ArboFichier";
+            this.Load += new System.EventHandler(this.ArboFichier_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CreationXML/ArboFichier.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CreationXML
+{
+    public partial class ArboFichier : Form
+    {
+        private string chemin;
+        private XmlDocument dom;
+        private frmMain main;
+
+        public ArboFichier(string Path, frmMain page)
+        {
+            InitializeComponent();
+            this.chemin = Path;
+            this.main = page;
+        }
+
+        private void ArboFichier_Load(object sender, EventArgs e)
+        {
+            // SECTION 1. Chargement du fichier XML
+            dom = new XmlDocument();
+            dom.Load(chemin);
+
+            // SECTION 2. Initialisation du TreeView
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(new TreeNode(dom.DocumentElement.Name));
+            TreeNode tNode = new TreeNode();
+            tNode = treeView1.Nodes[0];
+
+            // SECTION 3. Remplissage du treeView
+            AddNode(dom.DocumentElement, tNode);
+            treeView1.ExpandAll();
+
+        }
+
+        private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xNode;
+            TreeNode tNode;
+            XmlNodeList nodeList;
+
+            nodeList = inXmlNode.ChildNodes;
+
+            for (int i = 0; i <= nodeList.Count - 1; i++)
+            {
+                if (inXmlNode.Name == "QUESTION")
+                    inTreeNode.Text = inXmlNode.SelectSingleNode("LIBELLE").InnerText;
+                else
+                {
+                    xNode = inXmlNode.ChildNodes[i];
+
+                    if (inXmlNode.Name == "NIVEAU")
+                        inTreeNode.Text = inXmlNode.Name + " " + inXmlNode.Attributes["niv"].Value;
+
+                    inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
+
+                    tNode = inTreeNode.Nodes[i];
+                    AddNode(xNode, tNode);
+                }
+            }
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            XmlNodeList liste = dom.GetElementsByTagName("QUESTION");
+            foreach (XmlNode a in liste)
+            {
+                if (a.SelectSingleNode("LIBELLE").InnerText == e.Node.Text) // Si l'element selectionné est bien une question
+                {
+                    // Verif de la bonne reponse
+                    XmlNodeList lstChoix = a.SelectNodes("CHOIX"); // liste des choix de reponse de la question
+                    int idRep = 0;
+                    for (int i = 0; i < lstChoix.Count - 1; i++) // Parcours de la liste des choix
+                    {
+                        string valHachee = lstChoix[i].Attributes["reponse"].Value;
+                        string valeur = main.hachageString("true", lstChoix[i].InnerText).ToUpper(); // appel de la méthode cryptage pour le test
+                        if (valHachee.Equals(valeur))
+                            idRep = i;
+                    }
+                    main.recupDonnees(a.ParentNode.ParentNode.Name, Int32.Parse(a.ParentNode.Attributes["niv"].Value), a.SelectSingleNode("LIBELLE").InnerText, a.SelectNodes("CHOIX").Item(0).InnerText, a.SelectNodes("CHOIX").Item(1).InnerText, a.SelectNodes("CHOIX").Item(2).InnerText, idRep, a.SelectSingleNode("DESCRIPTION").InnerText);
+                    this.Close();
+                }
+            }
+        }
+    }
+}
